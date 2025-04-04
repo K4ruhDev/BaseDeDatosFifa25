fd764d55d43049970fbff04f52c5da090c3babf6
Karuh <jcperarnau06@proton.me>
2025-04-04 22:44:37 +0200

    Crear script para sacar los datos de los jugadores para importar directamente a la base de datos teniendo todos los equipos creados

---
+++ py/datosJugadores.py
@@ -0,0 +1,37 @@
+# Script usado para crear el CSV de jugadores para la base de datos usando pandas
+import pandas as pd
+
+# Leer los archivos CSV
+jugadores = pd.read_csv('../csv/general_players.csv')
+equipos = pd.read_csv('../csv/teams.csv')
+
+
+jugadores['Team'] = jugadores['Team'].str.strip()
+equipos['equipo'] = equipos['equipo'].str.strip()
+
+# Crear un diccionario para mapear los nombres de los equipos a sus IDs
+equipoToId = dict(zip(equipos['equipo'], equipos['id_equipo']))
+
+# Añadir la columna 'idEquipo' a los jugadores usando el diccionario
+jugadores['idEquipo'] = jugadores['Team'].map(equipoToId)
+
+## print(jugadores[['Team', 'idEquipo']]) Print para debug
+
+"""
+Columnas del CSV Jugadores
+Unnamed: 0.1,Unnamed: 0,Rank,Name,RAT,PAC,SHO,PAS,DRI,DEF,PHY,Position,Weak foot,Preferred foot,Height,Nation,League,Team,url,idEquipo
+"""
+
+columnasBorrar = [
+    'Rank', 'RAT', 'PAC', 'SHO', 'PAS',
+    'DRI', 'DEF', 'PHY', 'Position', 'Weak foot', 'Team',
+    'League', 'url'
+]
+
+nuevosJugadores = jugadores.drop(columns=columnasBorrar)
+
+nuevosJugadores.to_csv('../csv/jugadoresDB.csv', index=False)
+
+
+
+
