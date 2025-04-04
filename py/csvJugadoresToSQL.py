a7cb2833f38ec799149e7290515fe24189e81acf
Karuh <jcperarnau06@proton.me>
2025-04-04 23:24:09 +0200

    Insert Jugadores sql a partir de Python

--- py/csvJugadoresToSQL.py
+++ py/csvJugadoresToSQL.py
@@ -1,4 +1,5 @@
 import csv
+# Script para generar los inserts SQL a partir del CSV de jugadores
 
 csvJugadores = '../csv/jugadoresDB.csv'
 tabla = 'jugadores'
@@ -8,3 +9,22 @@ with open(csvJugadores, 'r', encoding='utf-8') as csvfile:
     # Leer la primera fila para obtener los nombres de las columnas
     columnas = next(reader)
     print(columnas)
+    
+    inserts = []
+
+    for fila in reader:
+        valores = []
+        for valor in fila:
+            valorReal = valor.replace("'", "''")  
+            valores.append(f"'{valorReal}'")
+
+        insert = f"INSERT INTO {tabla} ({', '.join(columnas)}) VALUES ({', '.join(valores)});"
+        inserts.append(insert)
+
+
+# Guardar los inserts en un archivo SQL
+with open('../sql/insertJugadores.sql', 'w', encoding='utf-8') as sqlfile:
+    for insert in inserts:
+        sqlfile.write(insert + '\n')
+    print(f"Se han generado {len(inserts)} sentencias INSERT.")
+
