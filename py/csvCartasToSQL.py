e582f35ddd420d364515d995f414a404b7f042c6
Gerson Baque <157021659+gersonb202@users.noreply.github.com>
2025-04-07 01:15:03 +0200

    Nuevo archivo py para la introducción de las cartas

--- py/csvCartasToSQL.py
+++ py/csvCartasToSQL.py
@@ -1,5 +1,5 @@
 import csv
-# Script para generar los inserts SQL a partir del CSV de jugadores
+# Script para generar los inserts SQL a partir del CSV de general_players
 
 def es_numero(valor):
     try:
@@ -8,18 +8,18 @@ def es_numero(valor):
     except ValueError:
         return False
 
-csvJugadores = '../csv/jugadoresDB.csv'
-tabla = 'jugadores'
-
-with open('../txt/english.txt', 'r', encoding='utf-8') as txtFile:
-    countries = [line.strip() for line in txtFile]
+csvCartas = '../csv/general_players.csv'
+tabla = 'cartas'
 
 with open('../txt/castellano.txt', 'r', encoding='utf-8') as txtFile:
-    paises = [line.strip() for line in txtFile]
+    castellano = [line.strip() for line in txtFile]
 
-traductor = dict(zip(countries, paises))
+with open('../txt/english.txt', 'r', encoding='utf-8') as txtFile:
+    english = [line.strip() for line in txtFile]
 
-with open(csvJugadores, 'r', encoding='utf-8') as csvfile:
+traductor = dict(zip(english, castellano))
+
+with open(csvCartas, 'r', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
     # Leer la primera fila para obtener los nombres de las columnas
     columnas = next(reader)
@@ -36,7 +36,6 @@ with open(csvJugadores, 'r', encoding='utf-8') as csvfile:
             elif es_numero(valor):
                 valores.append(valor)
             else:
-                # Si no quieres traducir los nombres de los paises al español, puedes saltarte el siguiente if
                 if valor in traductor:
                     valor = traductor[valor]
                 valorReal = valor.replace("'", "''")    
@@ -44,12 +43,12 @@ with open(csvJugadores, 'r', encoding='utf-8') as csvfile:
 
         insert = f"INSERT INTO {tabla} ({', '.join(columnas)}) VALUES ({', '.join(valores)});"
         inserts.append(insert)
-print(inserts)
+
 
 # Guardar los inserts en un archivo SQL
-with open('../sql/insertJugadores.sql', 'w', encoding='utf-8') as sqlfile:
+"""with open('../sql/insertCartas.sql', 'w', encoding='utf-8') as sqlfile:
     for insert in inserts:
         sqlfile.write(insert + '\n')
-    print(f"Se han generado {len(inserts)} sentencias INSERT.")
+    print(f"Se han generado {len(inserts)} sentencias INSERT.")"""
 
 
