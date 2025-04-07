a9a218e1e4425d256584aa99b619065773d2cee3
Gerson Baque <157021659+gersonb202@users.noreply.github.com>
2025-04-08 00:54:12 +0200

    Resuelto archivo de scrapping de precio

--- py/scrappingPrecio.py
+++ py/scrappingPrecio.py
@@ -1,14 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-url = 'https://www.cmtracker.net/players/238794'
+def asignar_precio(codigo):
+    url = 'https://www.cmtracker.net/players/{codigo}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    divs = soup.find_all('div', class_='Playerv2_player__main_info_box_item__6loQN')
+    div = divs[1]
+    span = div.find('span')
+    if span:
+        precio = span.text.strip()
+        if 'k' in precio:
+            return precio.replace('k', '') + '000'
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-divs = soup.find_all('div', class_='Playerv2_player__main_info_box_item__6loQN')
-
-div = divs[1]
-span = div.find('span')
-if span:
-    print(span.text)
+def main():
+    archivo_csv = '../csv/cartasJugadores.csv'
+    cartas = pd.read_csv(archivo_csv)
+    cartas.columns = cartas.columns.str.strip() 
+    if 'codigo_fifa' in cartas.columns:
+        # Generar ademas un precio a la cartas
+        cartas['precio'] = cartas['codigo_fifa'].apply(asignar_precio)
+        cartas.to_csv('../csv/cartasJugadores.csv', index=False)
+    
+if __name__ == "__main__":
+    main()
